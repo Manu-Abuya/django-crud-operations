2256a037dff153df7e8829b8065c46e46c08e14e
Manu-Abuya <manuabuya69@gmail.com>
2023-05-15 06:08:42 +0300

    updates

--- employee/urls.py
+++ employee/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+from .views import edit_employee
+
 urlpatterns = [
     path('', views.employees_list, name='employees-list'),
     path('create/', views.create_employee, name='create-employee'),
-    path('edit/', views.edit_employee, name='edit-employee'),
+    path('edit/<int:employee_id>', edit_employee, name='edit-employee'),
     path('delete/', views.delete_employee, name='delete-employee'),
 ]
